f44307a4a43020544a632a937799d2617ea21695
James Boland <jamespatrickboland@gmail.com>
2023-05-09 21:35:43 +0200

    Linear regression implemented in julia

--- linear_regression.jl
+++ linear_regression.jl
@@ -1,4 +1,4 @@
-using Pkg ; Pkg.add("Distributions") ; Pkg.add("CSV") ; Pkg.add("Tables")
+# using Pkg ; Pkg.add("Distributions") ; Pkg.add("CSV") ; Pkg.add("Tables")
 using Distributions
 using CSV, Tables
 
@@ -12,7 +12,19 @@ function rand_data(n)
     ϵ = rand(Truncated(Normal(0, 3), -5, 5), n)
     y = y_0 + ϵ
     data = reshape([feature ; y], n, 2)
-    CSV.write("reg_data.csv",  Tables.table(data), writeheader=false)
+    return data
 end
 
-rand_data(40)
+function lin_regression(data)
+    x = data[:, 1]
+    y = data[:, 2]
+    n = size(data)[1]
+    fullx = reshape([ones(n) ; x], n, 2)
+    fullx_t = transpose(fullx)
+    β = inv(fullx_t * fullx)*(fullx_t * y)
+    return β
+end
+
+data = rand_data(1000)
+lin_regression(data)
+
